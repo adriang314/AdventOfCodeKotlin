e952be023eef3048d3434ae356c39f0a49cb6812
Adrian Gredzinski <adrian314@o2.pl>
2023-12-13 17:54:42 +0100

    day 12 final

--- src/main/kotlin/solution/SolutionDay12.kt
+++ src/main/kotlin/solution/SolutionDay12.kt
@@ -1,15 +1,14 @@
 package solution
 
-import org.paukov.combinatorics3.Generator
-
-// 46778017609071 no
-private val cache = mutableMapOf<String, Long>()
-
 class SolutionDay12 : BaseSolution() {
+
     override val day = 12
 
     override fun task1(): String {
-        return ""
+        val rawLines = input().split("\r\n", "\n")
+        val lines = rawLines.map { Line(it) }
+        val x = lines.map { it.result }
+        return x.sum().toString()
     }
 
     override fun task2(): String {
@@ -19,21 +18,20 @@ class SolutionDay12 : BaseSolution() {
             val types = split[0]
             val groups = split[1]
             val newTypes = "$types?$types?$types?$types?$types"
-            val newGroups = """${groups},${groups},${groups},${groups},${groups}"""
+            val newGroups = "${groups},${groups},${groups},${groups},${groups}"
             Line("$newTypes $newGroups")
-           // Line(it)
         }
 
         val x = lines.map { it.result }
         return x.sum().toString()
     }
 
+    data class CacheKey(val typeIndex: Int, val groupIndex: Int, val damaged: Int)
 
     data class Group(val index: Int, val size: Int)
 
-
     class Line(val line: String) {
-
+        private val cache = mutableMapOf<CacheKey, Long>()
         private var groupLine: String
         private var typesLine: String
         private val numberRegex = Regex("(\\d+)")
@@ -46,7 +44,6 @@ class SolutionDay12 : BaseSolution() {
             groupLine = split[1]
             groups = numbers(numberRegex, groupLine).mapIndexed { index, i -> Group(index, i) }
             result = countForGroup(0, 0, 0)
-            cache[line] = result
         }
 
         private fun Char?.damaged() = this == '#'
@@ -58,32 +55,37 @@ class SolutionDay12 : BaseSolution() {
             currentGroupIdx: Int,
             damaged: Int
         ): Long {
+            val cacheKey = CacheKey(currentTypeIdx, currentGroupIdx, damaged)
+            val cachedResult = cache[cacheKey]
+            if (cachedResult != null)
+                return cachedResult
+
             val currentType = typesLine.getOrNull(currentTypeIdx)
             val currentGroup = groups.getOrNull(currentGroupIdx)
-
-            if (currentType == null && currentGroup == null)
-                return 1
-            if (currentType == null)
-                return 0
-
-            // check cache
-          //  val line = typesLine.substring(currentTypeIdx)
-          //  val group = if (groupLine.length >= currentGroupIdx * 2) groupLine.substring(currentGroupIdx * 2) else ""
-          //  val cachedKey = "$line $group"
-
-            //if (cache.containsKey(cachedKey) && damaged == 0)
-             //   return cache[cachedKey]!!
+            if (currentType == null && currentGroup == null) {
+                cache[cacheKey] = 1L
+                return 1L
+            }
+            if (currentType == null) {
+                cache[cacheKey] = 0L
+                return 0L
+            }
 
             if (currentType.damaged()) {
-                return handleDamaged(currentTypeIdx, currentGroupIdx, currentGroup, damaged)
+                val result = handleDamaged(currentTypeIdx, currentGroupIdx, currentGroup, damaged)
+                cache[cacheKey] = result
+                return result
             }
 
             if (currentType.operational()) {
-                return handleOperational(currentTypeIdx, currentGroupIdx, damaged)
+                val result = handleOperational(currentTypeIdx, currentGroupIdx, damaged)
+                cache[cacheKey] = result
+                return result
             }
 
             val ifDamaged = handleDamaged(currentTypeIdx, currentGroupIdx, currentGroup, damaged)
             val ifOperational = handleOperational(currentTypeIdx, currentGroupIdx, damaged)
+            cache[cacheKey] = ifDamaged + ifOperational
             return ifDamaged + ifOperational
         }
 
@@ -98,13 +100,11 @@ class SolutionDay12 : BaseSolution() {
             if (damaged > currentGroup.size)
                 return 0L
             if (damaged == currentGroup.size - 1) {
-
                 val nextType = typesLine.getOrNull(currentTypeIdx + 1)
                 if (nextType.operational() || nextType.unknown())
                     return countForGroup(currentTypeIdx + 2, currentGroupIdx + 1, 0)
                 if (nextType.damaged())
                     return 0
-
                 return countForGroup(currentTypeIdx + 1, currentGroupIdx + 1, 0)
             } else
                 return countForGroup(currentTypeIdx + 1, currentGroupIdx, damaged + 1)
@@ -117,7 +117,6 @@ class SolutionDay12 : BaseSolution() {
         ): Long {
             if (damaged > 0)
                 return 0L
-
             return countForGroup(currentTypeIdx + 1, currentGroupIdx, damaged)
         }
 
--- src/test/kotlin/Solution12Test.kt
+++ src/test/kotlin/Solution12Test.kt
@@ -6,36 +6,27 @@ class Solution12Test {
 
     @Test
     fun test(){
-
-//        assertEquals(1, Line("# 1").result)
-//        assertEquals(0, Line(". 1").result)
-//        assertEquals(1, Line("? 1").result)
-//        assertEquals(1, Line("?# 1").result)
-//        assertEquals(1, Line("?? 2").result)
-//        assertEquals(2, Line("?? 1").result)
-//        assertEquals(1, Line("?.? 1,1").result)
-//        assertEquals(2, Line("?? 1").result)
-//
-//
-//        //assertEquals(1, Line("?.? 1,1").result)
-//        assertEquals(2, Line("??.? 1,1").result)
-//        assertEquals(3, Line("???? 1,1").result)
-
-
-       // assertEquals(1, Line("???.###????.### 1,1,3,1,1,3").result)
-//        assertEquals(4, Line(".??..??...?##. 1,1,3").result)
-//        assertEquals(1, Line("?#?#?#?#?#?#?#? 1,3,1,6").result)
-//        assertEquals(1, Line("????.#...#... 4,1,1").result)
-//        assertEquals(4, Line("????.######..#####. 1,6,5").result)
-//        assertEquals(10, Line("?###???????? 3,2,1").result)
+        assertEquals(1, Line("# 1").result)
+        assertEquals(0, Line(". 1").result)
+        assertEquals(1, Line("? 1").result)
+        assertEquals(1, Line("?# 1").result)
+        assertEquals(1, Line("?? 2").result)
+        assertEquals(2, Line("?? 1").result)
+        assertEquals(1, Line("?.? 1,1").result)
+        assertEquals(2, Line("?? 1").result)
+        assertEquals(1, Line("?.? 1,1").result)
+        assertEquals(2, Line("??.? 1,1").result)
+        assertEquals(3, Line("???? 1,1").result)
+        assertEquals(1, Line("???.###????.### 1,1,3,1,1,3").result)
+        assertEquals(4, Line(".??..??...?##. 1,1,3").result)
+        assertEquals(1, Line("?#?#?#?#?#?#?#? 1,3,1,6").result)
+        assertEquals(1, Line("????.#...#... 4,1,1").result)
+        assertEquals(4, Line("????.######..#####. 1,6,5").result)
+        assertEquals(10, Line("?###???????? 3,2,1").result)
         assertEquals(4, Line(".??..??...?##.? 1,1,3").result)
         assertEquals(15, Line("?###????????? 3,2,1").result)
         assertEquals(1, Line("???.### 1,1,3").result)
-        assertEquals(1, Line("????.### 1,1,3").result)
+        assertEquals(3, Line("????.### 1,1,3").result)
         assertEquals(1, Line("???.###? 1,1,3").result)
-
-
-
-
     }
 }
