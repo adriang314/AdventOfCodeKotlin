1318f8ff202876d1d08bd97fa6903e87859a9f7a
Adrian Gredzinski <adrian314@o2.pl>
2024-01-13 14:47:24 +0100

    2021 day 3

---
+++ src/main/kotlin/year2021/SolutionDay03.kt
@@ -0,0 +1,76 @@
+package year2021
+
+import common.BaseSolution
+
+fun main() = println(SolutionDay03().result())
+
+class SolutionDay03 : BaseSolution() {
+
+    override val day = 3
+    override val year = 2021
+
+    override fun task1(): String {
+        val result = epsilon.toLong(2) * gamma.toLong(2)
+        return result.toString()
+    }
+
+    override fun task2(): String {
+        val result = oxygenGenerator.toLong(2) * co2.toLong(2)
+        return result.toString()
+    }
+
+    private var co2: String
+    private var oxygenGenerator: String
+    private var epsilon: String
+    private var gamma: String
+
+    init {
+        val lines = input().split("\r\n")
+        val length = lines.first().length
+
+        gamma = (0..<length).map { index ->
+            val zeros = lines.map { it[index] }.count { it == '0' }
+            val ones = lines.map { it[index] }.count { it == '1' }
+            if (ones == zeros)
+                throw Exception()
+            if (ones > zeros) '1' else '0'
+        }.joinToString("")
+
+        epsilon = (0..<length).map { index ->
+            val zeros = lines.map { it[index] }.count { it == '0' }
+            val ones = lines.map { it[index] }.count { it == '1' }
+            if (ones == zeros)
+                throw Exception()
+            if (ones < zeros) '1' else '0'
+        }.joinToString("")
+
+        oxygenGenerator = oxygenGeneratorFilter(lines, 0)
+        co2 = co2Filter(lines, 0)
+    }
+
+    private fun oxygenGeneratorFilter(lines: List<String>, index: Int): String {
+        if (lines.size == 1)
+            return lines.first()
+
+        val zeros = lines.map { Pair(it, it[index]) }.filter { it.second == '0' }
+        val ones = lines.map { Pair(it, it[index]) }.filter { it.second == '1' }
+
+        return if (ones.size >= zeros.size)
+            oxygenGeneratorFilter(ones.map { it.first }, index + 1)
+        else
+            oxygenGeneratorFilter(zeros.map { it.first }, index + 1)
+    }
+
+    private fun co2Filter(lines: List<String>, index: Int): String {
+        if (lines.size == 1)
+            return lines.first()
+
+        val zeros = lines.map { Pair(it, it[index]) }.filter { it.second == '0' }
+        val ones = lines.map { Pair(it, it[index]) }.filter { it.second == '1' }
+
+        return if (ones.size >= zeros.size)
+            co2Filter(zeros.map { it.first }, index + 1)
+        else
+            co2Filter(ones.map { it.first }, index + 1)
+    }
+}
