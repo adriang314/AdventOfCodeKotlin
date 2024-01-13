b11479803223b291f7c8ce4c0b5187f849a0451a
Adrian Gredzinski <adrian314@o2.pl>
2024-01-13 13:45:59 +0100

    2021 day 1

---
+++ src/main/kotlin/year2021/SolutionDay01.kt
@@ -0,0 +1,24 @@
+package year2021
+
+import common.BaseSolution
+
+fun main() = println(SolutionDay01().result())
+
+class SolutionDay01 : BaseSolution() {
+
+    override val day = 1
+    override val year = 2021
+
+    override fun task1() = depths.zipWithNext { a, b -> a < b }.count { it }.toString()
+
+    override fun task2() = depths.indices.mapNotNull {
+        val depth1 = depths.getOrNull(it)
+        val depth2 = depths.getOrNull(it + 1)
+        val depth3 = depths.getOrNull(it + 2)
+        if (depth1 != null && depth2 != null && depth3 != null)
+            depth1 + depth2 + depth3
+        else null
+    }.zipWithNext { a, b -> a < b }.count { it }.toString()
+
+    private var depths: List<Int> = input().split("\r\n").map { it.toInt() }
+}
