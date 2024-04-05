2fbd401c8afa9e7dcad5dc08e90ec6445adc96c0
Adrian Gredzinski <adrian314@o2.pl>
2024-04-05 12:02:29 +0200

    2019 day 1

---
+++ src/main/kotlin/year2019/SolutionDay01.kt
@@ -0,0 +1,32 @@
+package year2019
+
+import common.BaseSolution
+import kotlin.math.max
+
+fun main() = println(SolutionDay01().result())
+
+class SolutionDay01 : BaseSolution() {
+
+    override val day = 1
+    override val year = 2019
+
+    private val initValues = input().split("\r\n").map { it.toInt() }
+
+    override fun task1(): String {
+        val result = countFuel(initValues).sum()
+        return result.toString()
+    }
+
+    override fun task2(): String {
+        var values = initValues
+        var totalFuel = 0
+        do {
+            values = countFuel(values)
+            val fuelIncrement = values.sum()
+            totalFuel += fuelIncrement
+        } while (fuelIncrement > 0)
+        return totalFuel.toString()
+    }
+
+    private fun countFuel(values: List<Int>): List<Int> = values.map { max(0, (it / 3) - 2) }
+}
