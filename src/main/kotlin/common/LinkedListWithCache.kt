5d7de6f8c467c3ff05f4cc478301f88ef9b37a3a
Adrian Gredzinski <adrian314@o2.pl>
2024-11-24 22:02:22 +0100

    2018 day 9

---
+++ src/main/kotlin/common/LinkedListWithCache.kt
@@ -0,0 +1,165 @@
+package common
+
+class LinkedListWithCache<E> {
+    private val map = mutableMapOf<E, Node<E>>()
+    private var size = 0
+    private var head: Node<E>? = null
+    private var tail: Node<E>? = null
+
+    private class Node<E>(
+        var prev: Node<E>?,
+        var element: E,
+        var next: Node<E>?
+    )
+
+    fun getBefore(element: E, offset: Int): E {
+        var node = map[element]!!
+
+        repeat(offset) {
+            node = node.prev ?: tail!!
+        }
+
+        return node.element
+    }
+
+    fun getAfter(element: E, offset: Int): E {
+        var node = map[element]!!
+
+        repeat(offset) {
+            node = node.next ?: head!!
+        }
+
+        return node.element
+    }
+
+    fun getFirst() = head?.element
+
+    fun getLast() = tail?.element
+
+    fun removeFirst() = unlinkHead()
+
+    fun removeLast() = unlinkTail()
+
+    fun addFirst(element: E) = linkHead(element)
+
+    fun addLast(element: E) = linkTail(element)
+
+    fun add(element: E) = linkTail(element)
+
+    fun remove(element: E) = unlink(map[element]!!)
+
+    fun size() = size
+
+    fun addBefore(element: E, beforeElement: E) = linkBefore(element, map[beforeElement]!!)
+
+    operator fun contains(element: E) = map.containsKey(element)
+
+    private fun linkHead(element: E) {
+        val h = head
+        val newNode = Node(null, element, h)
+        head = newNode
+        if (h == null) {
+            tail = newNode
+        } else {
+            h.prev = newNode
+        }
+        size++
+        map[element] = newNode
+    }
+
+    private fun linkTail(element: E) {
+        val t = tail
+        val newNode = Node(t, element, null)
+        tail = newNode
+        if (t == null) {
+            head = newNode
+        } else {
+            t.next = newNode
+        }
+        size++
+        map[element] = newNode
+    }
+
+    private fun linkBefore(element: E, successor: Node<E>) {
+        val predecessor = successor.prev
+        val newNode = Node(predecessor, element, successor)
+        successor.prev = newNode
+        if (predecessor == null) {
+            head = newNode
+        } else {
+            predecessor.next = newNode
+        }
+        size++
+        map[element] = newNode
+    }
+
+    private fun unlinkHead() {
+        head?.let {
+            val next = it.next
+            it.next = null
+            head = next
+            if (next == null) {
+                tail = null
+            } else {
+                next.prev = null
+            }
+            size--
+        }
+    }
+
+    private fun unlinkTail() {
+        tail?.let {
+            val prev = it.prev
+            it.prev = null
+            tail = prev
+            if (prev == null) {
+                head = null
+            } else {
+                prev.next = null
+            }
+            size--
+        }
+    }
+
+    private fun unlink(curr: Node<E>): E {
+        val element = curr.element
+        val next = curr.next
+        val prev = curr.prev
+
+        if (prev == null) {
+            head = next
+        } else {
+            prev.next = next
+            curr.prev = null
+        }
+
+        if (next == null) {
+            tail = prev
+        } else {
+            next.prev = prev
+            curr.next = null
+        }
+
+        size--
+        return element
+    }
+
+    override fun toString(): String {
+        var curr = head
+        if (curr == null) return "[]"
+        else {
+            val sb = StringBuilder()
+            sb.append('[')
+            while (curr != null) {
+                sb.append(curr.element)
+                curr = curr.next
+                if (curr?.element == null) {
+                    sb.append(']')
+                } else {
+                    sb.append(',').append(' ')
+                }
+            }
+            return sb.toString()
+        }
+    }
+}
---
+++ src/main/kotlin/year2018/SolutionDay09.kt
@@ -0,0 +1,93 @@
+package year2018
+
+import common.BaseSolution
+import common.LinkedListWithCache
+
+fun main() = println(SolutionDay09().result())
+
+class SolutionDay09 : BaseSolution() {
+
+    override val day = 9
+    override val year = 2018
+
+    private val gameConfigRegex = Regex("^(\\d+) players; last marble is worth (\\d+) points$")
+    private val gameConfig1 = input().let {
+        val (players, marbles) = gameConfigRegex.find(it)!!.destructured
+        GameConfig(
+            (1..players.toInt()).map { id -> Player(id) },
+            (1..marbles.toInt()).map { id -> Marble(id) })
+    }
+
+    private val gameConfig2 = input().let {
+        val (players, marbles) = gameConfigRegex.find(it)!!.destructured
+        GameConfig(
+            (1..players.toInt()).map { id -> Player(id) },
+            (1..marbles.toInt() * 100).map { id -> Marble(id) })
+    }
+
+    override fun task1(): String {
+        return playGame(gameConfig1).toString()
+    }
+
+    override fun task2(): String {
+        return playGame(gameConfig2).toString()
+    }
+
+    private fun playGame(config: GameConfig): Long {
+        Game(config).play()
+        return config.players.maxOf { it.score }
+    }
+
+    private data class Game(val config: GameConfig) {
+        private val state = LinkedListWithCache<Marble>()
+        private val marble0 = Marble(0)
+        private var currentMarble = marble0
+
+        init {
+            state.add(marble0)
+        }
+
+        fun play() {
+            for (turn in 1..config.marbles.size) {
+                val marble = pickMarble(turn)
+                val player = pickPlayer(turn)
+
+                if (marble.isSpecial) {
+                    collectPoints(marble, player)
+                } else {
+                    addMarble(marble)
+                }
+            }
+        }
+
+        private fun collectPoints(marble: Marble, player: Player) {
+            player.score += marble.value
+            val marbleToRemove = state.getBefore(currentMarble, 7)
+            currentMarble = state.getAfter(marbleToRemove, 1)
+            state.remove(marbleToRemove)
+            player.score += marbleToRemove.value
+        }
+
+        private fun addMarble(marble: Marble) {
+            if (state.size() == 1) {
+                state.add(marble)
+            } else {
+                state.addBefore(marble, state.getAfter(currentMarble, 2))
+            }
+            currentMarble = marble
+        }
+
+        private fun pickPlayer(turn: Int) = config.players[(turn - 1) % config.players.size]
+
+        private fun pickMarble(turn: Int) = config.marbles[turn - 1]
+    }
+
+    private data class Marble(val value: Int) {
+        val isSpecial = value > 0 && value % 23 == 0
+        override fun toString() = "[$value]"
+    }
+
+    private data class Player(val id: Int, var score: Long = 0)
+
+    private data class GameConfig(val players: List<Player>, val marbles: List<Marble>)
+}
