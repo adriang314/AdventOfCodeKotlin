011178edff06d62d1276baea287a3b0ce6dd8801
Adrian Gredzinski <adrian314@o2.pl>
2024-12-17 07:08:39 +0100

    2024 day 17

--- src/main/kotlin/common/NumberExtensions.kt
+++ src/main/kotlin/common/NumberExtensions.kt
@@ -1,5 +1,9 @@
 package common
 
+fun Int.pow(exponent: Int) = power(this.toLong(), exponent)
+
+fun Long.pow(exponent: Int) = power(this, exponent)
+
 fun Long.numberOfDigits(): Int =
     when (this) {
         in -9L..9L -> 1
@@ -15,3 +19,11 @@ fun Int.numberOfDigits(): Int =
 fun Long.isEven(): Boolean = this % 2L == 0L
 
 fun Int.isEven(): Boolean = this % 2 == 0
+
+private fun power(base: Long, exponent: Int): Long {
+    var result: Long = 1
+    repeat(exponent) {
+        result *= base
+    }
+    return result
+}
