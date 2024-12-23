0010ec8a0b0db0be2195be895d4be51eca25850a
Adrian Gredzinski <adrian314@o2.pl>
2024-12-23 19:41:43 +0100

    2021 day 15 moved to Grid class

--- common/src/main/kotlin/common/GraphExtensions.kt
+++ common/src/main/kotlin/common/GraphExtensions.kt
@@ -2,10 +2,19 @@ package common
 
 import org.jgrapht.GraphPath
 import org.jgrapht.alg.shortestpath.BidirectionalDijkstraShortestPath
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath
 import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.DefaultWeightedEdge
 import org.jgrapht.graph.SimpleDirectedGraph
+import org.jgrapht.graph.SimpleDirectedWeightedGraph
 
 fun SimpleDirectedGraph<String, DefaultEdge>.addVertex(
+    position: Position,
+) {
+    addVertex(position.toString())
+}
+
+fun SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>.addVertex(
     position: Position,
 ) {
     addVertex(position.toString())
@@ -16,6 +25,28 @@ fun SimpleDirectedGraph<String, DefaultEdge>.addEdge(
     to: Position,
 ) {
     addEdge(from.toString(), to.toString())
+}
+
+fun SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>.addEdge(
+    from: Position,
+    to: Position,
+) {
+    addEdge(from.toString(), to.toString())
+}
+
+fun SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>.setEdgeWeight(
+    from: Position,
+    to: Position,
+    weight: Double
+) {
+    setEdgeWeight(from.toString(), to.toString(), weight)
+}
+
+fun DijkstraShortestPath<String, DefaultWeightedEdge>.getPath(
+    from: Position,
+    to: Position
+): GraphPath<String, DefaultWeightedEdge>? {
+    return getPath(from.toString(), to.toString())
 }
 
 fun BidirectionalDijkstraShortestPath<String, DefaultEdge>.getPath(
--- year2021/src/main/kotlin/year2021/SolutionDay15.kt
+++ year2021/src/main/kotlin/year2021/SolutionDay15.kt
@@ -1,59 +1,54 @@
 package year2021
 
-import common.BaseSolution
-import common.Point
-import common.PointMap
+import common.*
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath
 import org.jgrapht.graph.DefaultWeightedEdge
 import org.jgrapht.graph.SimpleDirectedWeightedGraph
-
-private typealias DirectedGraph = SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>
-private typealias Vertex = Point<SolutionDay15.State, Int>
 
 fun main() = println(SolutionDay15().result())
 
 class SolutionDay15 : BaseSolution() {
 
     override val day = 15
-    
+
     override fun task1(): String {
         val algorithm = DijkstraShortestPath(graph)
-        val shortestPath = algorithm.getPath(startPoint!!.id, endPoint!!.id)
+        val shortestPath = algorithm.getPath(startPoint.position, endPoint.position)!!
         return shortestPath.weight.toInt().toString()
     }
 
     override fun task2(): String {
-        val algorithm = DijkstraShortestPath(extendedGraph)
-        val shortestPath = algorithm.getPath(extendedStartPoint!!.id, extendedEndPoint!!.id)
+        val algorithm = DijkstraShortestPath(extGraph)
+        val shortestPath = algorithm.getPath(extStartPoint.position, extEndPoint.position)!!
         return shortestPath.weight.toInt().toString()
     }
 
-    private val points: List<Vertex>
-    private val extendedPoints: List<Vertex>
-    private var startPoint: Vertex? = null
-    private var extendedStartPoint: Vertex? = null
-    private var endPoint: Vertex? = null
-    private var extendedEndPoint: Vertex? = null
-    private val graph = DirectedGraph(DefaultWeightedEdge::class.java)
-    private val extendedGraph = DirectedGraph(DefaultWeightedEdge::class.java)
+    private val points: List<Point>
+    private val extPoints: List<Point>
+    private lateinit var startPoint: Point
+    private lateinit var extStartPoint: Point
+    private lateinit var endPoint: Point
+    private lateinit var extEndPoint: Point
+    private val graph = SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge::class.java)
+    private val extGraph = SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge::class.java)
 
     init {
-        val input = input()
-        val extendedInput = extendInput(input)
-        val pointMap = PointMap(input, Char::digitToInt) { State() }
-        val extendedPointMap = PointMap(extendedInput, Char::digitToInt) { State() }
+        val pointMap = Grid(input()) { c, position -> Point(position, c) }
+        val extendedPointMap = Grid(extendInput(input())) { c, position -> Point(position, c) }
 
-        points = pointMap.points.flatten()
-        extendedPoints = extendedPointMap.points.flatten()
+        points = pointMap.values.toList()
+        extPoints = extendedPointMap.values.toList()
 
-        addVertices(graph, points, pointMap,
+        addVertices(
+            graph, points, pointMap,
             { point -> this.startPoint = point },
             { point -> this.endPoint = point })
-        addVertices(extendedGraph, extendedPoints, extendedPointMap,
-            { point -> this.extendedStartPoint = point },
-            { point -> this.extendedEndPoint = point })
+        addVertices(
+            extGraph, extPoints, extendedPointMap,
+            { point -> this.extStartPoint = point },
+            { point -> this.extEndPoint = point })
         addEdges(graph, points)
-        addEdges(extendedGraph, extendedPoints)
+        addEdges(extGraph, extPoints)
     }
 
     private fun extendInput(input: String): String {
@@ -80,41 +75,45 @@ class SolutionDay15 : BaseSolution() {
     }
 
     private fun addVertices(
-        graph: DirectedGraph,
-        points: List<Vertex>,
-        pointMap: PointMap<State, Int>,
-        setStartPoint: (Vertex) -> Unit,
-        setEndPoint: (Vertex) -> Unit,
+        graph: SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>,
+        points: List<Point>,
+        pointMap: Grid<Point>,
+        setStartPoint: (Point) -> Unit,
+        setEndPoint: (Point) -> Unit,
     ) {
-        points.forEach {
-            if (it.rowIdx == 0 && it.colIdx == 0)
-                setStartPoint(it)
-            if (it.rowIdx == pointMap.length - 1 && it.colIdx == pointMap.height - 1)
-                setEndPoint(it)
-            graph.addVertex(it.id)
+        points.forEach { point ->
+            val position = point.position
+            if (position.x == 0 && position.y == 0)
+                setStartPoint(point)
+            if (position.x == pointMap.width - 1 && position.y == pointMap.height - 1)
+                setEndPoint(point)
+            graph.addVertex(position)
         }
     }
 
-    private fun addEdges(graph: DirectedGraph, points: List<Vertex>) {
-        points.forEach {
-            if (it.left != null) {
-                graph.addEdge(it.id, it.left!!.id)
-                graph.setEdgeWeight(it.id, it.left!!.id, it.left!!.value.toDouble())
+    private fun addEdges(graph: SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>, points: List<Point>) {
+        points.forEach { point ->
+            val position = point.position
+            point.w?.let {
+                graph.addEdge(position, position.w())
+                graph.setEdgeWeight(position, position.w(), (it as Point).riskLvl)
             }
-            if (it.right != null) {
-                graph.addEdge(it.id, it.right!!.id)
-                graph.setEdgeWeight(it.id, it.right!!.id, it.right!!.value.toDouble())
+            point.e?.let {
+                graph.addEdge(position, position.e())
+                graph.setEdgeWeight(position, position.e(), (it as Point).riskLvl)
             }
-            if (it.up != null) {
-                graph.addEdge(it.id, it.up!!.id)
-                graph.setEdgeWeight(it.id, it.up!!.id, it.up!!.value.toDouble())
+            point.n?.let {
+                graph.addEdge(position, position.n())
+                graph.setEdgeWeight(position, position.n(), (it as Point).riskLvl)
             }
-            if (it.down != null) {
-                graph.addEdge(it.id, it.down!!.id)
-                graph.setEdgeWeight(it.id, it.down!!.id, it.down!!.value.toDouble())
+            point.s?.let {
+                graph.addEdge(position, position.s())
+                graph.setEdgeWeight(position, position.s(), (it as Point).riskLvl)
             }
         }
     }
 
-    data class State(val empty: String = "")
+    private class Point(position: Position, value: Char) : Cell(position, value) {
+        val riskLvl = value.digitToInt().toDouble()
+    }
 }
