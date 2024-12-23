25cbd801c8d7145413e4735bd26d54eade390906
Adrian Gredzinski <adrian314@o2.pl>
2024-12-23 18:48:31 +0100

    2021 day 9 moved to Grid class

--- year2021/src/main/kotlin/year2021/SolutionDay09.kt
+++ year2021/src/main/kotlin/year2021/SolutionDay09.kt
@@ -1,23 +1,21 @@
 package year2021
 
-import common.BaseSolution
-import common.Point
-import common.PointMap
+import common.*
 
 fun main() = println(SolutionDay09().result())
 
 class SolutionDay09 : BaseSolution() {
 
     override val day = 9
-    
+
     override fun task1(): String {
-        val riskLevel = points.filter { it.state.isLowPoint }.sumOf { it.value + 1 }
+        val riskLevel = map.values.filter { it.isLowPoint }.sumOf { it.height + 1 }
         return riskLevel.toString()
     }
 
     override fun task2(): String {
-        val result = points
-            .groupBy { it.state.basinNumber }
+        val result = map.values
+            .groupBy { it.basinNumber }
             .asSequence()
             .filter { it.key != null }
             .map { it.value.size }
@@ -27,38 +25,34 @@ class SolutionDay09 : BaseSolution() {
         return result.toString()
     }
 
-    private var pointMap = PointMap(input(), Char::digitToInt) { Attribute() }
-    private val points = pointMap.points.flatten()
+    private var map = Grid(input()) { value, position -> Point(position, value) }
 
     init {
-        points.forEach { point ->
-            point.state.isLowPoint = point.neighbours().all { it.value > point.value }
-            point.state.isTopPoint = point.value == 9
+        map.values.forEach { point ->
+            point.isLowPoint = point.neighbours().all { it.value > point.value }
+            point.isTopPoint = point.height == 9
         }
 
-        markBasins()
-    }
-
-    private fun markBasins() {
+        // mark basins
         var basinNumber = 1
         do {
-            val basinPoint = points.firstOrNull { it.state.basinNumber == null && !it.state.isTopPoint }
-            if (basinPoint != null)
-                markBasin(basinPoint, basinNumber++)
+            val basinPoint = map.values.firstOrNull { it.basinNumber == null && !it.isTopPoint }
+            basinPoint?.markBasin(basinNumber++)
         } while (basinPoint != null)
     }
 
-    private fun markBasin(point: Point<Attribute, Int>, basinNumber: Int) {
-        if (point.state.isTopPoint || point.state.basinNumber != null)
-            return
+    private class Point(position: Position, value: Char) : Cell(position, value) {
+        val height = value.digitToInt()
+        var isLowPoint: Boolean = false
+        var basinNumber: Int? = null
+        var isTopPoint: Boolean = false
 
-        point.state.basinNumber = basinNumber
-        point.neighbours().forEach { markBasin(it, basinNumber) }
+        fun markBasin(basinNumber: Int) {
+            if (isTopPoint || this.basinNumber != null)
+                return
+
+            this.basinNumber = basinNumber
+            neighbours().forEach { (it as Point).markBasin(basinNumber) }
+        }
     }
-
-    data class Attribute(
-        var isLowPoint: Boolean = false,
-        var basinNumber: Int? = null,
-        var isTopPoint: Boolean = false,
-    )
 }
