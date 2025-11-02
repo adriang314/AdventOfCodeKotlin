ac5babc7112d999dc7b2be376133b7725650f751
Adrian Gredzinski <adrian314@o2.pl>
2025-11-02 14:13:31 +0100

    2016 day 5

---
+++ year2016/src/main/kotlin/year2016/SolutionDay05.kt
@@ -0,0 +1,61 @@
+package year2016
+
+import common.BaseSolution
+import java.security.MessageDigest
+
+fun main() = println(SolutionDay05().result())
+
+class SolutionDay05 : BaseSolution() {
+
+    override val day = 5
+
+    override fun task1(): String {
+        return PasswordFinder.findWithEightCharsUsingPosition(input(), false)
+    }
+
+    override fun task2(): String {
+        return PasswordFinder.findWithEightCharsUsingPosition(input(), true)
+    }
+
+    private class PasswordFinder {
+        companion object {
+            private val md5Algorithm = MessageDigest.getInstance("MD5")
+
+            fun findWithEightCharsUsingPosition(input: String, positionFromHash: Boolean): String {
+                val passwordChars = mutableMapOf<Int, Char>()
+                var searchIndex = 0
+                var foundPasswordChars = 0
+                while (foundPasswordChars < 8) {
+                    val (index, hash) = findHashStartingWithFiveZeros(searchIndex, input)
+                    searchIndex = index + 1
+                    if (positionFromHash) {
+                        val passwordPosition = hash[5].digitToIntOrNull()
+                        if (passwordPosition in (0..7) && !passwordChars.containsKey(passwordPosition)) {
+                            passwordChars[passwordPosition!!] = hash[6]
+                            foundPasswordChars++
+                        }
+                    } else {
+                        passwordChars[foundPasswordChars] = hash[5]
+                        foundPasswordChars++
+                    }
+                }
+
+                return passwordChars.toSortedMap().values.joinToString("")
+            }
+
+            private fun findHashStartingWithFiveZeros(initialSuffix: Int, input: String): Pair<Int, String> {
+                var index = initialSuffix
+                while (true) {
+                    val toHash = input + index.toString()
+                    val hash = md5(toHash)
+                    if (hash.startsWith("00000")) {
+                        return Pair(index, hash)
+                    }
+                    index++
+                }
+            }
+
+            private fun md5(input: String) = md5Algorithm.digest(input.toByteArray()).toHexString()
+        }
+    }
+}
