298ef79296bf6323073cfdf396c9950fe626ed38
Adrian Gredzinski <adrian314@o2.pl>
2025-11-19 06:54:56 +0100

    Added module for year 2025

---
+++ year2025/src/main/kotlin/year2025/SolutionDay01.kt
@@ -0,0 +1,18 @@
+package year2025
+
+import common.BaseSolution
+
+fun main() = println(SolutionDay01().result())
+
+class SolutionDay01 : BaseSolution() {
+
+    override val day = 1
+
+    override fun task1(): String {
+        return ""
+    }
+
+    override fun task2(): String {
+        return ""
+    }
+}
