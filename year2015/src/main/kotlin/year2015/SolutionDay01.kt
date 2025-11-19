db459758f2d175e6958b318bf2a6b9715b6554cf
Adrian Gredzinski <adrian314@o2.pl>
2025-11-19 20:59:54 +0100

    2015 day 1

--- year2015/src/main/kotlin/year2015/SolutionDay01.kt
+++ year2015/src/main/kotlin/year2015/SolutionDay01.kt
@@ -9,10 +9,10 @@ class SolutionDay01 : BaseSolution() {
     override val day = 1
 
     override fun task1(): String {
-        return ""
+        return input().map { if (it == '(') 1 else -1 }.sum().toString()
     }
 
     override fun task2(): String {
-        return ""
+        return input().scanIndexed(Pair(0, 0)) { idx, pair, ch -> Pair(idx + 1, if (ch == '(') pair.second + 1 else pair.second - 1) }.first { it.second < 0 }.first.toString()
     }
 }
