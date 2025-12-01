8129d4d1f4b5fa77478f08057ddd10b1e6b0c818
Adrian Gredzinski <adrian314@o2.pl>
2025-12-01 19:57:43 +0100

    2025 day 1

--- year2025/src/main/kotlin/year2025/SolutionDay01.kt
+++ year2025/src/main/kotlin/year2025/SolutionDay01.kt
@@ -8,11 +8,62 @@ class SolutionDay01 : BaseSolution() {
 
     override val day = 1
 
+    private val moves = input().split("\r\n").map { line ->
+        val rotate = when (line[0]) {
+            'L' -> Rotate.Left
+            'R' -> Rotate.Right
+            else -> throw IllegalArgumentException("Invalid rotate direction: ${line[0]}")
+        }
+        val steps = line.substring(1).toInt()
+        Move(rotate, steps)
+    }
+
     override fun task1(): String {
-        return ""
+        var position = 50
+        var zeroCount = 0
+
+        moves.forEach { move ->
+            position = when (move.rotate) {
+                Rotate.Left -> position - move.steps
+                Rotate.Right -> position + move.steps
+            }
+
+            while (position < 0) position += 100
+            while (position >= 100) position -= 100
+            if (position == 0) zeroCount++
+        }
+
+        return zeroCount.toString()
     }
 
     override fun task2(): String {
-        return ""
+        var position = 50
+        var zeroCount = 0
+
+        moves.forEach { move ->
+            when (move.rotate) {
+                Rotate.Left -> {
+                    repeat(move.steps) {
+                        position--
+                        if (position < 0) position += 100
+                        if (position == 0) zeroCount++
+                    }
+                }
+
+                Rotate.Right -> {
+                    repeat(move.steps) {
+                        position++
+                        if (position >= 100) position -= 100
+                        if (position == 0) zeroCount++
+                    }
+                }
+            }
+        }
+
+        return zeroCount.toString()
     }
+
+    private data class Move(val rotate: Rotate, val steps: Int)
+
+    private enum class Rotate { Left, Right }
 }
